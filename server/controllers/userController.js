92f5bbb6481e571862b67ad018d963bcb56bc0b4
Kalpana <kalpanaujad8@gmail.com>
2026-02-15 20:53:39 +0530

    feat(auth): implement JWT verification middleware and isAuth endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -39,7 +39,6 @@ export const register = async (req,res) => {
    }
 }
 
-
 // Login User : /api/user/login
 
 export const login = async (req, res) => {
@@ -75,6 +74,18 @@ export const login = async (req, res) => {
       return res.json({success: true, user: {email: user.email, name:user.name, }})
 
    } catch (error){
+      console.log(error.message);
+      res.json({ success: false, message: error.message });
+   }
+}
+
+// Check Auth: /api/user/is-auth
+export const isAuth = async (req,res) => {
+   try{
+      const {userId} = req.body;
+      const user = await User.findById(userId).select("-password")
+      return res.json({success: true, user});
+   }catch (error){
       console.log(error.message);
       res.json({ success: false, message: error.message });
    }
