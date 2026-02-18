a28586dba3e5fb80bc8f94ca6a91780b17bda99c
Kalpana <kalpanaujad8@gmail.com>
2026-02-18 22:21:37 +0530

    implement Cloudinary storage integration with Multer for image uploads

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 // mongodb called
 await connectDB();
+await connectCloudinary();
 
 // Middleware configuration
 app.use(express.json());
